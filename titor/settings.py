651aa51fa78e95c570fb5722c48c59609852cc4f
craftpaperbag <kazu11518@gmail.com>
2016-06-15 22:35:08 +0900

    dj_database_urlを使用して、herokuの時のデータベースに対応

--- titor/settings.py
+++ titor/settings.py
@@ -59,13 +59,17 @@ WSGI_APPLICATION = 'titor.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'titor' in hostname:
+    DATABASES = {
+        'default': dj_database_url.config()
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
