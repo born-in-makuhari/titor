8627bb7842e0e4a8e6c6955c440e4d9210ab2d37
yoshinari21 <yoshinari21@gmail.com>
2016-06-19 22:30:58 +0900

    demo-chart

--- titor/settings.py
+++ titor/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'titorApp',
+    'chartjs',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,3 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    '/home/yoshinari/wk/python-django/Workspace/titor/titorApp/templates',
+)
---
+++ titorApp/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+from random import shuffle, randint
+from itertools import islice
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView
+from chartjs.colors import next_color, COLORS
+from chartjs.views.columns import BaseColumnsHighChartsView
+from chartjs.views.lines import BaseLineChartView, HighchartPlotLineChartView
+from chartjs.views.pie import HighChartPieView, HighChartDonutView
+
+from django.utils.translation import ugettext_lazy as _
+# Create your views here.
+
+class ColorsView(TemplateView):
+    template_name = 'colors.html'
+
+    def get_context_data(self, **kwargs):
+        data = super(ColorsView, self).get_context_data(**kwargs)
+        data['colors'] = islice(next_color(), 0, 50)
+        return data
+
+
+
+class ChartMixin(object):
+    def get_labels(self):
+        """Return 7 labels."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_data(self):
+        """Return 3 random dataset to plot."""
+        def data():
+            """Return 7 randint between 0 and 100."""
+            return [randint(0, 100) for x in range(7)]
+
+        return [data() for x in range(3)]
+
+    def get_colors(self):
+        """Return a new shuffle list of color so we change the color
+        each time."""
+        colors = COLORS[:]
+        shuffle(colors)
+        return next_color(colors)
+
+class ColumnHighChartJSONView(ChartMixin, BaseColumnsHighChartsView):
+    title = _('Column Highchart test')
+    yUnit = '%'
+    providers = ['All']
+    credits = {"enabled": False}
+
+    def get_data(self):
+        return [super(ColumnHighChartJSONView, self).get_data()]
+
+
+
+class LineChartJSONView(ChartMixin, BaseLineChartView):
+    pass
+
+class LineHighChartJSONView(ChartMixin, HighchartPlotLineChartView):
+    # special - line charts credits are personalized
+    credits = {
+        'enabled': True,
+        'href': 'http://example.com',
+        'text': 'Novapost Team',
+    }
+
+
+
+class PieHighChartJSONView(ChartMixin, HighChartPieView):
+    pass
+
+
+class DonutHighChartJSONView(ChartMixin, HighChartDonutView):
+    pass
+
+
+class AboutView(TemplateView):
+    template_name = "titor/graph.html"
+line_chart_json = LineChartJSONView.as_view()
+line_highchart_json = LineHighChartJSONView.as_view()
+pie_highchart_json = PieHighChartJSONView.as_view()
+donut_highchart_json = DonutHighChartJSONView.as_view()
