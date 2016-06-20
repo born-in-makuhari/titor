1901aa98865af3ce1e4806d3260f8cd10601ec05
craftpaperbag <kazu11518@gmail.com>
2016-06-20 22:30:39 +0900

    settingsを参照

--- titor/urls.py
+++ titor/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
@@ -7,11 +8,11 @@ urlpatterns = patterns('',
 )
 
 if not settings.DEBUG:
-      urlpatterns += patterns(
-          '',
-          (
-              r'^static/(?P<path>.*)$',
-              'django.views.static.serve',
-              {'document_root': settings.STATIC_ROOT}
-          ),
-      )
+    urlpatterns += patterns(
+        '',
+        (
+            r'^static/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.STATIC_ROOT}
+        ),
+    )
