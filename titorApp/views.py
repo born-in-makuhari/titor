c28fae64282929c1bbbee51ea5118b1a84ff2b7f
yoshinari21 <yoshinari21@gmail.com>
2016-07-02 07:19:08 +0900

    1ヶ月株価取得

--- titorApp/views.py
+++ titorApp/views.py
@@ -17,12 +17,10 @@ import jsm
 import datetime
 import re
 # Create your views here.
-
 #GLOBAL
 CORP_CD = 998407
-START_DATE = datetime.date(2015, 5, 27)
-END_DATE = datetime.date(2016, 6, 17)
-print "GLOBAL"
+
+
 class ColorsView(TemplateView):
     template_name = 'colors.html'
 
@@ -34,17 +32,13 @@ class ColorsView(TemplateView):
 class ChartMixin(object):
 
     def test(self):
-        print "test"
         return 0 
 
     def get_labels(self):
         """Return 7 labels."""
-        #return ["January", "February", "March", "April", "May", "June", "July"]
-        get_jsm = JsmGetPrice(CORP_CD,START_DATE,END_DATE)
-        #get_jsm = GET_JSM
+        get_jsm = JsmGetPrice(CORP_CD)
         date = get_jsm.get_date()
         return date
-        #return ["1","2","3","4","5","6","7","8"]
 
     def get_data(self):
         """Return 3 random dataset to plot."""
@@ -53,7 +47,7 @@ class ChartMixin(object):
             return [randint(0, 100) for x in range(7)]
 
         #return [data() for x in range(3)]
-        get_jsm = JsmGetPrice(CORP_CD,START_DATE,END_DATE)
+        get_jsm = JsmGetPrice(CORP_CD)
         close_price = get_jsm.get_price()     
         return [close_price]
 
@@ -98,36 +92,32 @@ class AboutView(TemplateView):
     template_name = "titor/graph.html"
 
 class JsmGetPrice():
-    def __init__(self,corp_cd,start_date,end_date):
+    def __init__(self,corp_cd):
+        today = datetime.date.today()
+        old_day = today - datetime.timedelta(days=30) 
+        start_date = old_day
+        end_date = today
+        q = jsm.Quotes()
         self.corp_cd = corp_cd    
         self.start_date = start_date    
         self.end_date = end_date    
-        q = jsm.Quotes()
         self.close_dict = {}
         self.pricedata = q.get_historical_prices(corp_cd, jsm.DAILY, start_date, end_date)
+        self.close_list = []
+        self.date_list = []
         for pricedata_eachday in self.pricedata:
-        #print str(pricedata_eachday.date).replace(' 00:00:00','')
             self.close_dict[str(pricedata_eachday.date).replace(' 00:00:00','')] = pricedata_eachday.close
 
-    def get_price(self):
-        close_list = []
         for k,v in sorted(self.close_dict.items()):
-            close_list.append(v)
+            self.close_list.append(v)
+            self.date_list.append(k)
 
-        return close_list
+
+    def get_price(self):
+        return self.close_list
 
     def get_date(self):
-        #pricedata = q.get
-        #print self.pricedata
-        i = 0
-        day_list = []
-        print len(self.pricedata)
-        for pricedata_eachday in self.pricedata:
-            day_list.append(str(pricedata_eachday.date).replace(' 00:00:00',''))
-        
-        print day_list
-        return day_list
-
+        return self.date_list
 
 
 line_chart_json = LineChartJSONView.as_view()
